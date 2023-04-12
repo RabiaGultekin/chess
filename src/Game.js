d1d427f4a101bd30c37cd4f6f76af3b7bb4917c3
RabiaGultekin <rbnn.1995@gmail.com>
2023-04-12 16:44:36 +0300

    React chess part 3 done

--- src/Game.js
+++ src/Game.js
@@ -13,12 +13,36 @@ export const initGame = () => {
 
 export const move = (from, to) => {
     console.log(from, to);
-    const moveOperation = chess.move({from, to})
-    if(moveOperation){
+    const moveOperation = chess.move({ from, to })
+    if (moveOperation) {
         updateGame();
     }
 }
 
 const updateGame = () => {
-    subjectGame.next({chess: chess.board()})
+    const isGameOver = chess.isGameOver();
+    subjectGame.next({ chess: chess.board(), isGameOver, result: isGameOver ? getGameResult() : null })
+}
+
+const getGameResult = () => {
+    if (chess.isCheckmate()) { //oyun şah/matla mı bitti?
+        const winner = chess.turn() === 'w' ? 'Black' : 'White';
+        return `ŞAH MAT - Kazanan : ${winner}`
+    }
+    else if (chess.isDraw()) {
+        let reason = "50 Hamle Kuralı"
+        if (chess.isStalemate) { // Eğer satranç oyununda çıkmaz döngü oluşursa reason içerisine çıkmaz döngü at. Oyunun çıkmaz döngüye yakalandı mı?
+            reason = 'Çıkmaz Döngü'
+        }
+        else if (chess.isThreefoldRepetition) { //tekrarlanan hamleler
+            reason = 'Tekrarlanan Hamle'
+        }
+        else if (chess.isInsufficientMaterial) { //
+            reason = 'Yetersiz Malzeme'
+        }
+        return reason
+    }
+    else{
+        return 'Bilinmeyen Durum'
+    }
 }
